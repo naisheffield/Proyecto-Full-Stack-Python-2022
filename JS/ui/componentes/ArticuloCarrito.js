f6339eaf4ecc44155f4e9dac5d7ca23cf51af127
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2022-10-11 17:13:50 -0300

    agrega Clase para la creacion del componente Articulo de Carrito

--- JS/ui/componentes/ArticuloCarrito.js
+++ JS/ui/componentes/ArticuloCarrito.js
@@ -1,13 +1,46 @@
 class ArticuloCarrito {
   constructor(articuloData) {
-    // articuloData es la info obtenida de paquetes.json
-    // propiedades del objeto ArticuloCarrito para utilizar con el método render()
+    this.id = articuloData.id;
+    this.nombre = articuloData.nombre;
+    this.imageURL = articuloData.imagenes[0];
+    this.precio = articuloData.precio;
+    this.cantidad = articuloData.cantidad;
+    this.destino = articuloData.destino;
+    this.duracion = articuloData.duracion;
+    this.servicios = articuloData.servicios
   }
 
-  render(idElementoContenedor) {
-    const elementoContenedor = document.getElementById(idElementoContenedor);
+  crearElemento() {
+    const articuloContenedor = document.createElement("div");
+    articuloContenedor.className = "cart-item-container";
+    articuloContenedor.dataset.id = this.id;
 
-    elementoContenedor.innerHTML = `<h3>Articulo del carrito</h3>`;
+    const listadoServicios = this.servicios.map(servicio => `<li>${servicio}.</li>`).join("");
+
+    articuloContenedor.innerHTML = `
+      <div class="img-container">
+        <img class="card-img" src=${this.imageURL} alt="${this.destino.pais} item image">
+      </div>
+      <div class="item-info-container">
+        <div class="item-title">
+          <h3>${this.nombre}</h3>
+        </div>
+        <div>
+          <span>${this.cantidad} pasajes</span>
+          <span>${this.precio} US$ + impuestos c/u</span>
+        </div>
+        <div>
+          <span>${this.destino.ciudad} | ${this.destino.pais} | ${this.destino.continente}</span>
+          <span>${this.duracion.dias} días | ${this.duracion.noches} noches</span>
+        </div>
+        <ul>
+          ${listadoServicios}
+        </ul>
+        <button>Eliminar</button>
+      </div>
+    `;
+
+    return articuloContenedor;
   }
 }
 
