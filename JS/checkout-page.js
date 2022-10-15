69a0a72c3499a06c835f6aa8d2828ff5b6f643ff
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2022-10-15 12:42:03 -0300

    agrega logica y estilos para Menu/Boton hamburguesa. Agregados a paginas Paquetes y Checkout

--- JS/checkout-page.js
+++ JS/checkout-page.js
@@ -1,7 +1,9 @@
 import { inicializarCheckout } from "./ui/carrito.js";
 import { inicializarBotonCarrito } from "./ui/botonCarrito.js";
+import { inicializarBotonBurgerMenu } from "./ui/botonBurgerMenu.js";
 
 function inicializar() {
+  inicializarBotonBurgerMenu();
   inicializarBotonCarrito();
   inicializarCheckout();
 }
--- JS/paquetes-page.js
+++ JS/paquetes-page.js
@@ -1,7 +1,9 @@
 import { inicializarListadoPaquetes } from "./ui/listadoPaquetes.js";
 import { inicializarBotonCarrito } from "./ui/botonCarrito.js";
+import { inicializarBotonBurgerMenu } from "./ui/botonBurgerMenu.js";
 
 function inicializar() {
+  inicializarBotonBurgerMenu();
   inicializarBotonCarrito();
   inicializarListadoPaquetes();
 }
