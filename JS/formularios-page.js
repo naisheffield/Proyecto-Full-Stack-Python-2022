ce3b8880732a1d761b03951fbb554f5d1b2595b0
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2022-10-22 11:29:10 -0300

    agrega validaciones restantes y las separa en unidades funcionales. Agrega validaciones en el HTML

---
+++ JS/formularios-page.js
@@ -0,0 +1,11 @@
+import { inicializarBotonCarrito } from "./ui/botonCarrito.js";
+import { inicializarBotonBurgerMenu } from "./ui/botonBurgerMenu.js";
+import { inicializarFormulario } from "./form/formulario.js";
+
+function inicializar() {
+  inicializarBotonCarrito();
+  inicializarBotonBurgerMenu();
+  inicializarFormulario();
+}
+
+inicializar();
