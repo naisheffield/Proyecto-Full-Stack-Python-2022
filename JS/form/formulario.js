597315e8c864b37703af624eccdc5a99b3b10a66
brendaborrego <brendaborrego00@gmail.com>
2022-10-24 21:43:15 -0300

    agrega API para formularios

--- JS/form/formulario.js
+++ JS/form/formulario.js
@@ -6,8 +6,16 @@ function inicializarFormulario() {
   form.addEventListener("submit", (e) => submitHandler(e));
 }
 
-function submitHandler(e) {
+async function submitHandler(e) {
   e.preventDefault();
+  var data = new FormData(e.target);
+  fetch(e.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  })
     
   const formType = form.dataset.type;
   let formValido;
@@ -18,7 +26,13 @@ function submitHandler(e) {
   if (formValido) {
     // Una vez que el formulario se encuentre validado se hace la peticion al servidor
     // el código AJAX de Formspree iría dentro de este bloque de código
-  }
+    form.reset()
+    Swal.fire({
+      title: "Tu mensaje fue enviado!",
+      text: "Gracias por contactarte, nos comunicaremos con vos a la brevedad.",
+      icon: "success" ,
+     })
+}
 }
 
 export { inicializarFormulario };
