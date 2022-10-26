627daf290ea16bfa08ce1437b989b087513f99ec
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2022-10-26 13:14:25 -0300

    agrega logica al navbar del Header

--- JS/paquetes-page.js
+++ JS/paquetes-page.js
@@ -3,10 +3,12 @@ import { inicializarListadoPaquetes } from "./ui/listadoPaquetes.js";
 import { inicializarFiltroPequetes } from "./ui/filtroPaquetes.js";
 import { inicializarBotonCarrito } from "./ui/botonCarrito.js";
 import { inicializarBotonBurgerMenu } from "./ui/botonBurgerMenu.js";
+import { inicializarNavbar } from "./ui/headerNavbar.js";
 
 async function inicializar() {
   const data = await obtenerDataDeJSON();
 
+  inicializarNavbar();
   inicializarListadoPaquetes(data);
   inicializarFiltroPequetes(data);
   inicializarBotonCarrito();
---
+++ JS/ui/headerNavbar.js
@@ -0,0 +1,11 @@
+
+function inicializarNavbar() {
+  const botonesNavbar = document.querySelectorAll("a");
+  const { pathname } = location;
+
+  botonesNavbar.forEach(btn => {
+    if(btn.pathname === pathname) btn.classList.add("on-page");
+  })
+}
+
+export { inicializarNavbar };
